d0c6aec83d576cd9f344a3a8a3228cf8a79d5781
Hamjaster <hamzasepal@gmail.com>
2024-01-22 14:56:11 +0500

    added .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://backend-of-inventory-management-system.vercel.app", // Replace with your API URL
+        // target: "https://backend-of-inventory-management-system.vercel.app", 
+        target: import.meta.env.VITE_API, // Replace with your API URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
